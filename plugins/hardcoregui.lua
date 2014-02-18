bc5a6451811a44d4d88aaba544aefc9c99a13587
Luke Spragg <luke@lukespragg.com>
2014-02-18 11:37:59 -0500

    Add version checking to Hardcore GUI

--- plugins/hardcoregui.lua
+++ plugins/hardcoregui.lua
@@ -1,5 +1,5 @@
 PLUGIN.Title = "Hardcore GUI"
-PLUGIN.Version = "0.2.1"
+PLUGIN.Version = "0.2.2"
 PLUGIN.Description = "Forcefully hides the player stat bars, inventory, and hotbar slots."
 PLUGIN.Author = "Luke Spragg - Wulfspider"
 PLUGIN.Url = "http://forum.rustoxide.com/resources/140/"
@@ -8,10 +8,13 @@ function PLUGIN:Init()
     -- Log that plugin is loading
     print(self.Title .. " v" .. self.Version .. " loading...")
 
+    -- Run update check
+    self:GetLatestVersion()
+
     -- Continuously trigger event to hide the GUI
     timer.Repeat(0.1, 0, function() self:TimerEvent() end)
 
-    -- Log tha plugin has loaded
+    -- Log that plugin has loaded
     print(self.Title .. " v" .. self.Version .. " loaded!")
 end
 
@@ -40,6 +43,28 @@ function PLUGIN:TimerEvent()
                     end
                 end
             end
+        end
+    end
+end
+
+function PLUGIN:GetLatestVersion()
+    -- Get latest version from URL
+    webrequest.Send("https://raw2.github.com/Wulfspider/OxidePlugins/master/versions/hardcoregui.txt", function(code, response)
+        self:UpdateCheck(code, response)
+    end)
+end
+
+function PLUGIN:UpdateCheck(code, response)
+    local pattern = "%d%.%d+" -- Ex. 0.1.0 or 0.2
+    local compare = string.find(response, pattern) -- Compare response to version pattern
+
+    -- Check for valid latest version
+    if (compare ~= nil) then
+        local latest = response
+        -- Check if new version is available
+        if (self.Version < latest) then
+            -- Report update available to server log
+            print(self.Title .. " is outdated! Installed version: " .. self.Version .. ", Latest version: " .. latest)
         end
     end
 end
